963df730dc06e8e650f6ed68c34449b1e302071b
scmsqhn <qhn614@126.com>
2018-06-04 17:34:43 +0800

    add python file

--- bond_risk/__init__.py
+++ bond_risk/__init__.py
@@ -1,4 +1,8 @@
-# this is bond-risk handle package
-# author: liwen qinhaining
-import load_mysqL_from_localcpk
-import data_helper
+#
+import os 
+CUR_PATH = os.path.join(os.environ['DMPPATH'], 'iba/dmp/gongan/storm_crim_classify/extcode')
+import sys
+sys.path.append(CUR_PATH)
+current_dir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(current_dir)
+sys.path.append("..")
